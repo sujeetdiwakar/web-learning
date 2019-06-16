4a8b022650bcbb2c022b6634ea0c061a7d3cad9c
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-06-16 11:11:32 +0530

    Form Data using Ajax serialize() method
    Jquery Post Form Data using Ajax serialize() method

---
+++ 25-jquery-insert-form-data-using-ajax-serialize-method/tbl_form.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2019 at 10:00 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_form`
+--
+
+CREATE TABLE `tbl_form` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `message` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_form`
+--
+
+INSERT INTO `tbl_form` (`id`, `name`, `message`) VALUES
+(1, 'Sumit', 'Hello'),
+(2, 'Max Shop', 'HI'),
+(3, 'Esho', 'Hi'),
+(4, 'sujeet', 'Hi'),
+(5, 'Amit', 'Hello');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_form`
+--
+ALTER TABLE `tbl_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_form`
+--
+ALTER TABLE `tbl_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
